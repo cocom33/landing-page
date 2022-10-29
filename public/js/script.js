6987751ee90c7e98f325456b8deb27e0d9bee068
cocom33 <ancintcococ3@gmail.com>
2022-10-29 08:30:42 +0700

    landing page

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+// navbar fixed
+window.onscroll = function (){
+	const header = document.querySelector('header');
+	const fixedNav = header.offsetTop;
+	
+	if(window.pageYOffset > fixedNav) {
+		header.classList.add('navbar-fixed');
+	} else {
+		header.classList.remove('navbar-fixed');
+	}
+}
+
+// haburger
+const hamburger = document.querySelector('#hamburger');
+const navbar = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+	hamburger.classList.toggle('hamburger-active');
+	navbar.classList.toggle('hidden');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
